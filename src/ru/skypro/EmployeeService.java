950bec13091bf1cb1231ac181481b249327b0767
Telion <www.neon@mail.ru>
2022-08-18 10:08:11 +0500

    решил задание повышенной сложности, исправил методы на статические

--- src/ru/skypro/EmployeeService.java
+++ src/ru/skypro/EmployeeService.java
@@ -2,7 +2,7 @@ package ru.skypro;
 
 public class EmployeeService {
 
-    public Employee[] createEmployeeList() {
+    public static Employee[] createEmployeeList() {
         Employee[] employees = new Employee[10];
         employees[0] = new Employee("Иванов Петр Ильич", 1, 100_000);
         employees[1] = new Employee("Петров Илья Иванович", 1, 130_000);
@@ -15,7 +15,7 @@ public class EmployeeService {
         return employees;
     }
 
-    public void printAllEmployees(Employee[] employees) {
+    public static void printAllEmployees(Employee[] employees) {
         for (Employee employee : employees) {
             if (employee != null) {
                 System.out.println("    " + employee);
@@ -23,7 +23,7 @@ public class EmployeeService {
         }
     }
 
-    public double calculateSalaryAmount(Employee[] employees) {
+    public static double calculateSalaryAmount(Employee[] employees) {
         double salaryAmount = 0;
         for (Employee employee : employees) {
             if (employee != null) {
@@ -33,7 +33,7 @@ public class EmployeeService {
         return salaryAmount;
     }
 
-    public double calculateSalaryAmountByDepartment(Employee[] employees, int department) {
+    public static double calculateSalaryAmountByDepartment(Employee[] employees, int department) {
         double salaryAmount = 0;
         for (Employee employee : employees) {
             if (employee != null && employee.getDepartment() == department) {
@@ -43,7 +43,7 @@ public class EmployeeService {
         return salaryAmount;
     }
 
-    public Employee findMinSalaryEmployee(Employee[] employees) {
+    public static Employee findMinSalaryEmployee(Employee[] employees) {
         double minSalary = Double.MAX_VALUE;
         Employee minSalaryEmployee = null;
         for (Employee employee : employees) {
@@ -57,7 +57,7 @@ public class EmployeeService {
         return minSalaryEmployee;
     }
 
-    public Employee findMinSalaryEmployeeByDepartment(Employee[] employees, int department) {
+    public static Employee findMinSalaryEmployeeByDepartment(Employee[] employees, int department) {
         double minSalary = Double.MAX_VALUE;
         Employee minSalaryEmployee = null;
         for (Employee employee : employees) {
@@ -71,7 +71,7 @@ public class EmployeeService {
         return minSalaryEmployee;
     }
 
-    public Employee findMaxSalaryEmployee(Employee[] employees) {
+    public static Employee findMaxSalaryEmployee(Employee[] employees) {
         double maxSalary = 0;
         Employee maxSalaryEmployee = null;
         for (Employee employee : employees) {
@@ -85,7 +85,7 @@ public class EmployeeService {
         return maxSalaryEmployee;
     }
 
-    public Employee findMaxSalaryEmployeeByDepartment(Employee[] employees, int department) {
+    public static Employee findMaxSalaryEmployeeByDepartment(Employee[] employees, int department) {
         double maxSalary = 0;
         Employee maxSalaryEmployee = null;
         for (Employee employee : employees) {
@@ -99,13 +99,22 @@ public class EmployeeService {
         return maxSalaryEmployee;
     }
 
-    public double calculateAverageSalary(Employee[] employees) {
-        double salaryAmount = this.calculateSalaryAmount(employees);
-        int employeesCount = this.calculeteEmployeesCount(employees);
+    public static double calculateAverageSalary(Employee[] employees) {
+        double salaryAmount = EmployeeService.calculateSalaryAmount(employees);
+        int employeesCount = EmployeeService.calculateEmployeesCount(employees);
         return (double) Math.round(salaryAmount / employeesCount * 100) / 100;
     }
 
-    private int calculeteEmployeesCount(Employee[] employees) {
+    public static double calculateAverageSalaryByDepartment(Employee[] employees, int department) {
+        double salaryAmount = EmployeeService.calculateSalaryAmountByDepartment(employees, department);
+        int employeesCount = EmployeeService.calculateEmployeesCountByDepartment(employees, department);
+        if (employeesCount != 0) {
+            return (double) Math.round(salaryAmount / employeesCount * 100) / 100;
+        }
+        return 0;
+    }
+
+    public static int calculateEmployeesCount(Employee[] employees) {
         int employeesCount = 0;
         for (Employee employee : employees) {
             if (employee != null) {
@@ -115,7 +124,17 @@ public class EmployeeService {
         return employeesCount;
     }
 
-    public void printAllFIOEmployees(Employee[] employees) {
+    public static int calculateEmployeesCountByDepartment(Employee[] employees, int department) {
+        int employeesCount = 0;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getDepartment() == department) {
+                employeesCount++;
+            }
+        }
+        return employeesCount;
+    }
+
+    public static void printAllFIOEmployees(Employee[] employees) {
         for (Employee employee : employees) {
             if (employee != null) {
                 System.out.println("    " + employee.getFullName());
@@ -127,7 +146,6 @@ public class EmployeeService {
         if (increasePercent < 0) {
             throw new IllegalArgumentException("Неверное значение");
         }
-
         for (Employee employee : employees) {
             if (employee != null) {
                 double currSalary = employee.getSalary();
@@ -137,26 +155,96 @@ public class EmployeeService {
         }
     }
 
-    public void printEmployeeWithMinSalaryByDepartment(Employee[] employees, int deparment) {
-        Employee minSalaryEmployeeByDepartment = this.findMinSalaryEmployeeByDepartment(employees, deparment);
+    public static void increaseSalaryByDepartment(Employee[] employees, int increasePercent, int department) {
+        Employee[] employeesFromDepartment = new Employee[employees.length];
+        int employeeNumber = 0;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getDepartment() == department) {
+                employeesFromDepartment[employeeNumber] = employee;
+                employeeNumber++;
+            }
+        }
+        EmployeeService.increaseSalary(employeesFromDepartment, increasePercent);
+    }
+
+    public static void printEmployeeWithMinSalaryByDepartment(Employee[] employees, int department) {
+        Employee minSalaryEmployeeByDepartment = EmployeeService.findMinSalaryEmployeeByDepartment(employees, department);
         if (minSalaryEmployeeByDepartment == null) {
-            System.out.println("В отделе №" + deparment + " не найдены сотрудники");
+            System.out.println("В отделе №" + department + " не найдены сотрудники");
         } else {
-            System.out.println("2.a. Сотрудник с минимальной зарплатой, в отделе №" + deparment + ": " + minSalaryEmployeeByDepartment);
+            System.out.println("2.a. Сотрудник с минимальной зарплатой в отделе №" + department + ": " + minSalaryEmployeeByDepartment);
         }
     }
 
-    public void printEmployeeWithMaxSalaryByDepartment(Employee[] employees, int deparment) {
-        Employee maxSalaryEmployeeByDepartment = this.findMaxSalaryEmployeeByDepartment(employees, deparment);
+    public static void printEmployeeWithMaxSalaryByDepartment(Employee[] employees, int department) {
+        Employee maxSalaryEmployeeByDepartment = EmployeeService.findMaxSalaryEmployeeByDepartment(employees, department);
         if (maxSalaryEmployeeByDepartment == null) {
-            System.out.println("В отделе №" + deparment + " не найдены сотрудники");
+            System.out.println("В отделе №" + department + " не найдены сотрудники");
         } else {
-            System.out.println("2.b. Сотрудник с максимальной зарплатой, в отделе №" + deparment + ": " + maxSalaryEmployeeByDepartment);
+            System.out.println("2.b. Сотрудник с максимальной зарплатой в отделе №" + department + ": " + maxSalaryEmployeeByDepartment);
         }
     }
 
-    public void printSalaryAmountByDepartment(Employee[] employees, int department) {
-        double salaryAmountByDepartment = this.calculateSalaryAmountByDepartment(employees, department);
-        System.out.println("2.c. Сумма затрат на зарплату, по отделу №" + department + ": " + salaryAmountByDepartment);
+    public static void printSalaryAmountByDepartment(Employee[] employees, int department) {
+        double salaryAmountByDepartment = EmployeeService.calculateSalaryAmountByDepartment(employees, department);
+        System.out.println("2.c. Сумма затрат на зарплату по отделу №" + department + ": " + salaryAmountByDepartment);
+    }
+
+    public static void printAverageSalaryByDepartment(Employee[] employees, int department) {
+        double averageSalaryByDepartment = EmployeeService.calculateAverageSalaryByDepartment(employees, department);
+        System.out.println("2.d. Средняя зарплата по отделу №" + department + ": " + averageSalaryByDepartment);
+    }
+
+    public static void printAllEmployeesByDepartment(Employee[] employees, int department) {
+        System.out.println("2.f. Все сотрудники отдела №" + department);
+        for (Employee employee : employees) {
+            if (employee != null && employee.getDepartment() == department) {
+                System.out.println("    " + employee.getId() + ", " + employee.getFullName() + ", " + employee.getSalary());
+            }
+        }
+    }
+
+    public static Employee[] findEmployeesBySalaryLower(Employee[] employees, double baseSalary) {
+        Employee[] foundEmployees = new Employee[employees.length];
+        int employeeNumber = 0;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() < baseSalary) {
+                foundEmployees[employeeNumber] = employee;
+                employeeNumber++;
+            }
+        }
+        return foundEmployees;
+    }
+
+    public static void printEmployeesBySalaryLower(Employee[] employees, double baseSalary) {
+        System.out.println("3.a. Все сотрудники с зарплатой меньше " + baseSalary);
+        Employee[] employeesBySalaryLower = EmployeeService.findEmployeesBySalaryLower(employees, baseSalary);
+        for (Employee employee : employeesBySalaryLower) {
+            if (employee != null) {
+                System.out.println("    " + employee);
+            }
+        }
+    }
+
+    public static Employee[] findEmployeesBySalaryHigher(Employee[] employees, double baseSalary) {
+        Employee[] foundEmployees = new Employee[employees.length];
+        int employeeNumber = 0;
+        for (Employee employee : employees) {
+            if (employee != null && employee.getSalary() >= baseSalary) {
+                foundEmployees[employeeNumber] = employee;
+                employeeNumber++;
+            }
+        }
+        return foundEmployees;
+    }
+
+    public static void printEmployeesBySalaryHigher(Employee[] employees, double baseSalary) {
+        System.out.println("3.a. Все сотрудники с зарплатой больше или равно " + baseSalary);
+        Employee[] employeesBySalaryHigher = EmployeeService.findEmployeesBySalaryHigher(employees, baseSalary);
+        for (Employee employee : employeesBySalaryHigher) {
+            if (employee != null) {
+                System.out.println("    " + employee);
+            }
+        }
     }
 }
--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,33 +3,37 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        EmployeeService employeeService = new EmployeeService();
-        Employee[] employees = employeeService.createEmployeeList();
-        double salaryAmount = employeeService.calculateSalaryAmount(employees);
-        Employee minSalaryEmployee = employeeService.findMinSalaryEmployee(employees);
-        Employee maxSalaryEmployee = employeeService.findMaxSalaryEmployee(employees);
-        double averageSalary = employeeService.calculateAverageSalary(employees);
+        Employee[] employees = EmployeeService.createEmployeeList();
+        double salaryAmount = EmployeeService.calculateSalaryAmount(employees);
+        Employee minSalaryEmployee = EmployeeService.findMinSalaryEmployee(employees);
+        Employee maxSalaryEmployee = EmployeeService.findMaxSalaryEmployee(employees);
+        double averageSalary = EmployeeService.calculateAverageSalary(employees);
 
         System.out.println("--- Уровень 1. Базовая сложность ---");
         System.out.println("a. Получить список всех сотрудников");
-        employeeService.printAllEmployees(employees);
+        EmployeeService.printAllEmployees(employees);
         System.out.println("b. Сумма затрат на зарплаты в месяц: " + salaryAmount + " рублей");
         System.out.println("c. Сотрудник с минимальной зарплатой: " + minSalaryEmployee);
         System.out.println("d. Сотрудник с максимальной зарплатой: " + maxSalaryEmployee);
         System.out.println("e. Среднее значение зарплат: " + averageSalary);
         System.out.println("f. Получить ФИО всех сотрудников:");
-        employeeService.printAllFIOEmployees(employees);
+        EmployeeService.printAllFIOEmployees(employees);
         System.out.println();
 
         System.out.println("--- Уровень 2. Повышенная сложность ---");
         System.out.println("1. После индексации зарплат:");
         EmployeeService.increaseSalary(employees, 10);
-        employeeService.printAllEmployees(employees);
-
-        employeeService.printEmployeeWithMinSalaryByDepartment(employees, 2);
-        employeeService.printEmployeeWithMaxSalaryByDepartment(employees, 5);
-        employeeService.printSalaryAmountByDepartment(employees, 1);
-
+        EmployeeService.printAllEmployees(employees);
+        EmployeeService.printEmployeeWithMinSalaryByDepartment(employees, 2);
+        EmployeeService.printEmployeeWithMaxSalaryByDepartment(employees, 5);
+        EmployeeService.printSalaryAmountByDepartment(employees, 1);
+        EmployeeService.printAverageSalaryByDepartment(employees, 5);
+        System.out.println("2.e. Проиндексировать зарплату сотрудников в отделе:");
+        EmployeeService.increaseSalaryByDepartment(employees, 7, 3);
+        EmployeeService.printAllEmployees(employees);
+        EmployeeService.printAllEmployeesByDepartment(employees, 2);
+        EmployeeService.printEmployeesBySalaryLower(employees, 70_000);
+        EmployeeService.printEmployeesBySalaryHigher(employees, 70_000);
     }
 
 
